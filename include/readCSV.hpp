1fe0fc3b5c123d375bfbcb83ceac6d8946149e0d
Legonesa <146031407+Legonesa@users.noreply.github.com>
2026-07-16 00:06:33 +0300

    Update readCSV.hpp

--- include/readCSV.hpp
+++ include/readCSV.hpp
@@ -7,19 +7,19 @@
 #include <map>
 #include <variant>
 
-class DataSet{
+class DataSet {
 public:
-    DataSet(const char *fileName);
-    void readCSV();
-    void readCSVinRange(int begin, int end);
-    void readCSVRow(int row);
-    void readCSVCategory(std::string category);
-    void readCSVCategoryinRange(std::string category, int begin, int end);
-    void readCSVCategoryinRow(std::string category, int row);
-    long double getValueNum(std::string category, int row);
-    std::string getValueStr(std::string category, int row);
-    std::string getCategoryName(int CategoryIndex);
-    void freeMemory();
+    DataSet(const char* fileName); // Opens the file.
+    void readCSV(); // Reads the file.
+    void readCSVinRange(int begin, int end); // Reads a specific range of rows.
+    void readCSVRow(int row); // Reads a single row.
+    void readCSVCategory(std::string category); // Reads a single category (column).
+    void readCSVCategoryinRange(std::string category, int begin, int end); // Reads a specific range of rows within a category.
+    void readCSVCategoryinRow(std::string category, int row); // Reads a single row within a category.
+    long double getValueNum(std::string category, int row); // Returns a parsed value. (Numeric value.)
+    std::string getValueStr(std::string category, int row); // Returns a parsed value. (String value.)
+    std::string getCategoryName(int CategoryIndex); // Returns the name of the category at the specified index.
+    void freeMemory(); // Clears the memory.
 private:
     const char* file;
     std::map<std::string, std::map<int, std::string>> values;
